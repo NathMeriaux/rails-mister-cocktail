37d84934f162dc9265cdf5e9ce8d039597c7f11f
Nath Meriaux <nath.meriaux@gmail.com>
2016-06-03 15:09:49 +0200

    Upload de photos ok

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,9 @@
 class Cocktail < ActiveRecord::Base
+  mount_uploader :photo, PhotoUploader
   has_many :doses, dependent: :destroy
   has_many :ingredients, :through => :doses
 
   validates :name, uniqueness: true, presence: true
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
   resources :doses, only: [:destroy]
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
 end
